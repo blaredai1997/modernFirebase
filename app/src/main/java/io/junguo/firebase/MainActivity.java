6e0f203fd7f1012daeae34cb75b47ae94b7180f1
gjuoun <gjuoun@gmail.com>
2018-06-04 16:37:53 -0300

    done : retrieve info from firebase

--- app/src/main/java/io/junguo/firebase/MainActivity.java
+++ app/src/main/java/io/junguo/firebase/MainActivity.java
@@ -32,37 +32,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //get Views by Id
         mFirebaseBtn = (Button) findViewById(R.id.firebase_btn);
         mNameField = (EditText) findViewById(R.id.name_field);
         mEmailField = (EditText) findViewById(R.id.email_field);
         mAgeField = (EditText) findViewById(R.id.age_field);
         mNameView = (TextView) findViewById(R.id.name_view);
 
-        // get database reference
-        mDatabase = FirebaseDatabase.getInstance().getReference();
+        // get database reference, here we use subtable named "users"
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("users");
 
-        // click to save user and email to Firebase
+        // Saved info to Firebase after clicked the button
         mFirebaseBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // 1 Create child in root object
+
                 String name = mNameField.getText().toString().trim();
                 String email = mEmailField.getText().toString().trim();
                 String age = mAgeField.getText().toString().trim();
 
+                //use hashmap to store info
                 HashMap<String, String> dataMap = new HashMap<>();
 
                 dataMap.put("Name", name);
                 dataMap.put("Email", email);
                 dataMap.put("Age", age);
 
+                // .push() will push dataMap to database, OnCompleteListener will effect after
                 mDatabase.push().setValue(dataMap).addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
+                        // clear fields when submit successfully
                         if (task.isSuccessful()) {
                             mNameField.setText("");
                             mEmailField.setText("");
+                            mAgeField.setText("");
 
+                            //jump up a toast to notify "Stored"
                             Toast.makeText(MainActivity.this, "Stored...", Toast.LENGTH_LONG).show();
 
                         } else {
@@ -73,28 +79,25 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        // retrieve data from Database , then show them on textView
+        // retrieve data from Database, then show them on textView
         mDatabase.addValueEventListener(new ValueEventListener() {
+
+            // 'onDataChange' method reloads data while changes happened in database
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-//                String email = dataSnapshot.child("Name").child("Email").getValue().toString();
-//                String age = dataSnapshot.child("Age").getValue().toString();
 
                 String name="", email="", age="";
                 String result = "";
 
                 for(DataSnapshot child: dataSnapshot.getChildren()){
-//                        if(child.getKey().equals("Name"))
-//                            name = child.getValue().toString();
-//                        if(child.getKey().equals("Email"))
-//                            email = child.getValue().toString();
-//                        if(child.getKey().equals("Age"))
-//                            age = child.getValue().toString();
-                    result = child.getKey();
+                    name = child.child("Name").getValue().toString();
+                    email = child.child("Email").getValue().toString();
+                    age = child.child("Age").getValue().toString();
 
-//                    result = result + "Email: " + email + ", age: " + age+ "\n";
+                    result = result + "Name: "+name + " - Email: " + email + " - age: " + age+ "\n";
                 }
 
+                //display result on NameView
                 mNameView.setText(result);
             }
 
